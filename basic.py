481c52cb419ad3cf2bb138fc6c5d40225e9a8698
bhoh <bhoh0320@gmail.com>
2023-12-24 22:09:43 +0900

    List 원소제거 내용 추가

---
+++ basic.py
@@ -0,0 +1,33 @@
+# List 타입
+
+a = [1,2,3]
+b = [4,5,6]
+
+print(a+b)
+print(a,b)
+
+my_list = ["유재석", "조세호", "박명수"]
+print(my_list)
+
+# 조세호가 몇 번째 칸에 있는가?
+print(my_list.index("조세호"))
+
+# List 원소 추가(append, insert(index, ))
+
+## 하하가 다음 칸에 탐
+my_list.append("하하")
+print(my_list)
+
+## 정형돈이 유재석과 조세호 사이에 탐
+my_list.insert(1, "정형돈")
+print(my_list)
+
+# List 원소 제거(pop, remove(특정값))
+
+## pop 뒤에서 한 명씩 꺼냄(return값이 존재)
+print(my_list.pop())
+print(my_list)
+
+## List 중간에 있는 특정값(정형돈)을 제거
+my_list.remove("정형돈")
+print(my_list)
