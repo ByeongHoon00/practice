42f18d963019c08d71498b72322c521f33710038
bhoh <bhoh0320@gmail.com>
2023-12-25 21:58:20 +0900

    if문 내용추가

---
+++ python(if,for,while 반복문)/if_practice.py
@@ -0,0 +1,10 @@
+# if문
+weather = input("오늘의 날씨는? ")  ## str형태를 입력을 받음
+if weather == "비" or weather == "눈":  ## 조건이 참이면 출력 아니면 넘어감
+    print("우산을 챙기세요")
+elif weather == "미세먼지":     ## 조건이 참이면 출력 아니면 넘어감
+    print("마스크를 챙기세요")
+else :
+    print("준비물이 필요없어요")
+
+# for문
