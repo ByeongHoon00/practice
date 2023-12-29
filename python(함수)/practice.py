6c99a87e83da9b8daf02f310c8639a310be5c11e
bhoh <bhoh0320@gmail.com>
2023-12-29 10:12:02 +0900

    함수의 기본값과 키워드값에 대한 내용추가

--- python(함수)/practice.py
+++ python(함수)/practice.py
@@ -11,12 +11,27 @@ def withdraw(balance, money):
         print("잔액이 부족합니다. 잔액은 {0}원입니다.".format(balance))
         return balance
 
-balance = 0 # 잔액
-balance = deposit(balance, 1000) # 입금
+balance = 0 ## 잔액
+balance = deposit(balance, 1000) ## 입금
 print(balance)
 
-balance = withdraw(balance, 800) # 출금
+balance = withdraw(balance, 800) ## 출금
 print(balance)
 
-balance = withdraw(balance, 800) # 출금
+balance = withdraw(balance, 800) ## 출금
 print(balance)
+
+# 기본값
+def profile(name, age=17, main_lang="python"):
+    print("이름 : {0}\t나이 : {1}\t주 사용 언어:{2}".format(name, age, main_lang))
+
+profile("유재석")
+profile("김태호")
+
+# 키워드값
+def profile2(name, age, main_lang):
+    print(name, age, main_lang)
+
+## 함수의 매개변수의 값을 키워드를 통해 전달받으면 순서가 바뀌어있어도 정상적으로 전달 가능함
+profile2(name="유재석", main_lang="python" , age=20)
+profile2(main_lang="java", age=25, name="김태호")
