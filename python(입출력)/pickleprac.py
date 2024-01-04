0ae1926d8f8a718f452d53a1994168ca17c2c366
bhoh <bhoh0320@gmail.com>
2024-01-04 11:57:06 +0900

    with에 대한 내용추가

--- python(입출력)/pickleprac.py
+++ python(입출력)/pickleprac.py
@@ -7,7 +7,18 @@ print(profile)
 pickle.dump(profile,profile_file) # profile에 있는 정보를 file에 저장
 profile_file.close() """
 
-profile_file = open("profile.pickle", "rb")
+""" profile_file = open("profile.pickle", "rb")
 profile = pickle.load(profile_file) # file에 있는 정보를 profile에 불러오기
 print(profile)
-profile_file.close()
+profile_file.close() """
+
+## with
+with open("profile.pickle", "rb") as profile_file:
+    print(pickle.load(profile_file))
+
+with open("study.txt", "w", encoding="utf8") as study_file:
+    study_file.write("공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
+
