0dc4c5e087bd8211a72d85f8d17ebfaa7d12b46e
bhoh <bhoh0320@gmail.com>
2024-01-18 18:26:42 +0900

    패키지, 모듈의 위치 확인에 대한 내용추가

--- packageprac.py
+++ packageprac.py
@@ -13,3 +13,10 @@ trip_to = vietnam.VietnamPackage()
 trip_to.detail()
 trip_to2 = thailand.ThailandPackage()
 trip_to2.detail()
+
+# import inspect를 활용해 패키지, 모듈의 위치를 확인할 수 있음
+
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
