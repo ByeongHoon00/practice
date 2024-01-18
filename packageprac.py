e75dbb9edd53ff860d7125d68ec4bf11825af6d3
bhoh <bhoh0320@gmail.com>
2024-01-18 18:05:32 +0900

    all에 대한 내용추가

--- packageprac.py
+++ packageprac.py
@@ -1,7 +1,15 @@
-import travel.thailand
-trip_to = travel.thailand.ThailandPackage()
-trip_to.detail()
+# __init__.py는 from import *로 패키지를 호출 할 때 어느 모듈을 공개할지 결정한다.
 
-from travel import vietnam
-trip_to2 = vietnam.VietnamPackage()
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to2 = vietnam.VietnamPackage()
+# trip_to2.detail()
+
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
+trip_to2 = thailand.ThailandPackage()
 trip_to2.detail()
