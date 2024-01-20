8240a1a760620bd1c66b798496e7e85766a6fcc1
bhoh <bhoh0320@gmail.com>
2024-01-20 17:38:18 +0900

    내장함수에 대한 내용추가

---
+++ python(함수)/fprac.py
@@ -0,0 +1,14 @@
+# 내장함수 - import없이 사용가능한 함수(ex : input, dir등)
+## input - 사용자 입력을 받는 함수
+language = input("무슨 언어를 좋아하세요?")
+print("{0}은 아주 좋은 언어입니다!".format(language))
+
+## dir - 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 갖고 있는지 표시
+print(dir())
+import random # 외장함수
+print(dir())
+import pickle
+print(dir())
+print(dir(random))
+lst = [1,2,3]
+print(dir(lst))
